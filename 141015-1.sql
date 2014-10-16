2bedd73852756689aa19c72bc18f05abeb7ab9d8
s13008 <s13008@std.it-coollege.ac.jp>
2014-10-16 13:45:17 +0900

    sql修正

--- 141015-1.sql
+++ 141015-1.sql
@@ -1,3 +1,5 @@
+alter session set nls_date_format = 'DD-MON-RR';
+alter session set nls_date_language = 'AMERICAN';
 SELECT ename, hiredate, deptno
 FROM employees
 WHERE deptno = '10'
--- 141015-2.sql
+++ 141015-2.sql
@@ -1,3 +1,5 @@
+alter session set nls_date_format = 'RR-MM-DD';
+alter session set nls_date_language = 'JAPANESE';
 SELECT ename, hiredate, deptno
 FROM employees
 WHERE deptno = '10'
